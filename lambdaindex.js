9b27ef94b8ecf33ae3aec28db3ec9126e8c7ea38
N.S.Devaraj <nsdevaraj@gmail.com>
2018-06-28 15:56:39 +0530

    fixes

--- lambdaindex.js
+++ lambdaindex.js
@@ -88,8 +88,15 @@ const SessionEndedRequestHandler = {
   },
 };
 
+
 const ErrorHandler = {
   canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
       .speak('Sorry, I can\'t understand the command. Please say again.')
       .reprompt('Sorry, I can\'t understand the command. Please say again.')
       .getResponse();
